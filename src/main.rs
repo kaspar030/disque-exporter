66fe4e5bbb3ba0be54b2c2237270af0dd90be8fe
Kaspar Schleiser <kaspar@schleiser.de>
2023-03-09 13:49:19 +0100

    add "disque_" prefix, add some help, use gethostname

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use disque::{Disque, FromRedisValue};
-
-use metrics::{absolute_counter, gauge};
-use rouille::router;
-
-use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
 use std::env;
 
+use disque::{Disque, FromRedisValue};
+use gethostname::gethostname;
+use metrics::{absolute_counter, describe_counter, describe_gauge, gauge};
+use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
+use rouille::router;
+
 fn main() {
-    let listen_address = env::var("DISQUE_EXPORTER_LISTEN_ADDR").unwrap_or("127.0.0.1:8000".into());
+    let listen_address = env::var("DISQUE_EXPORTER_LISTEN_ADDR").unwrap_or("127.0.0.1:9090".into());
     let disque_url =
         env::var("DISQUE_EXPORTER_DISQUE_URL").unwrap_or("redis://localhost:7711".into());
-    let host = env::var("DISQUE_EXPORTER_HOST").unwrap_or(listen_address.clone());
+    let host =
+        env::var("DISQUE_EXPORTER_HOST").unwrap_or_else(|_| gethostname().into_string().unwrap());
 
     tracing_subscriber::fmt::init();
 
@@ -43,12 +44,37 @@ fn main() {
             let idle = u64::from_redis_value(stats.get("idle").unwrap()).unwrap() as f64;
             let blocked = u64::from_redis_value(stats.get("blocked").unwrap()).unwrap() as f64;
 
+            describe_counter!(
+                "disque_queue_jobs_in_total",
+                "increments every time a job is enqueued for any reason"
+            );
             absolute_counter!("disque_queue_jobs_in_total", jobs_in, "queue" => name.clone());
+
+            describe_counter!(
+                "disque_queue_jobs_out_total",
+                "increments every time a job is dequeued for any reason"
+            );
             absolute_counter!("disque_queue_jobs_out_total", jobs_out, "queue" => name.clone());
 
+            describe_gauge!(
+                "disque_queue_age_seconds",
+                "time since this queue was created"
+            );
             gauge!("disque_queue_age_seconds", age, "queue" => name.clone());
+
+            describe_gauge!(
+                "disque_queue_blocked_workers",
+                "the number of clients blocked on this queue right now"
+            );
             gauge!("disque_queue_blocked_workers", blocked, "queue" => name.clone());
+
+            describe_gauge!("disque_queue_idle_seconds", "time this queue has been idle");
             gauge!("disque_queue_idle_seconds", idle, "queue" => name.clone());
+
+            describe_gauge!(
+                "disque_queue_len_jobs",
+                "number of jobs currently queued in this queue"
+            );
             gauge!("disque_queue_len_jobs", len, "queue" => name.clone());
         }
         handle.render()
